215da3dec1830979ba4ee8acedd10b55514752e8
Rosny Ratsianompo <rosny.rat@onja.org>
2020-11-24 15:44:01 +0000

    worked on reusable react 1

--- pages/App.js
+++ pages/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 
 function App() {
 	return (<div className="App">
-	<header className="App-header">
-	  <HelloWorld />
-	</header>
-  </div>);
+		<header className="App-header">
+			<HelloWorld />
+		</header>
+	</div>);
 }
 
 function HelloWorld() {
 	return <h1>Hello World!</h1>
-  }
+}
 
 export default App;
